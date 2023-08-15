11a5f42762edb842f1d2b32ad208a4d8eaae5b9a
mariacavalc <mariaacavalc@gmail.com>
2023-08-15 00:15:33 -0400

    adding GET /pessoas?t=[:termo da busca]

--- src/main/java/org/madu/controller/PessoaController.java
+++ src/main/java/org/madu/controller/PessoaController.java
@@ -3,6 +3,7 @@ package org.madu.controller;
 import jakarta.servlet.http.HttpServletResponse;
 import org.madu.database.service.PessoaService;
 import org.madu.entities.Pessoa;
+import org.madu.exception.BadRequestException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,11 +11,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.UUID;
 
+import static java.util.Objects.isNull;
 import static org.madu.util.RequestValidator.validate;
 
 @RestController
@@ -32,6 +36,15 @@ public class PessoaController {
         service.insertPessoa(pessoa);
 
         response.addHeader("Location", "/pessoas/" + pessoa.getId().toString());
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<Pessoa> queryPessoa(@RequestParam("t") String searchTerm) {
+        if (isNull(searchTerm) || searchTerm.isEmpty() || searchTerm.isBlank()){
+            throw new BadRequestException();
+        }
+        return service.queryPessoa(searchTerm);
     }
 
     @GetMapping(value = "/{id}")
